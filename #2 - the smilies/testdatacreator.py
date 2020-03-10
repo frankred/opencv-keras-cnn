1604429c8b285088331ff96c3ba4b1bc89571296
Frank Roth <hellofrank@inmag.world>
2020-03-10 19:32:01 +0100

    Adding samples

--- #2 - the smilies/testdatacreator.py
+++ #2 - the smilies/testdatacreator.py
@@ -8,6 +8,8 @@ SAME_IMAGE_DEPTH_AS_ORIGINAL = -1
 
 def run():
     source_image_path = 'resources/no-smilies.jpg'
+    target_folder = 'smilie-no-smilie'
+
     moving_size_slow = 1
     moving_size_fast = 10
     rect_x = 400
@@ -16,7 +18,6 @@ def run():
     rect_h = 26
     rect_thickness = 1
     rect_border_color = (255, 0, 0)
-    target_folder = 'happy/0'
     target_counter = 1
 
     source_image = cv.imread(cv.samples.findFile(source_image_path), cv.IMREAD_COLOR)
@@ -38,22 +39,23 @@ def run():
         if (c == 2490368):
             rect_y = rect_y - moving_size_slow
 
-        if (c == 56):
+        if (c == 56 or c == 119):
             rect_y = rect_y - moving_size_fast
 
-        if (c == 54):
+        if (c == 54 or c == 100):
             rect_x = rect_x + moving_size_fast
 
-        if (c == 50):
+        if (c == 50 or c == 115):
             rect_y = rect_y + moving_size_fast
 
-        if (c == 52):
+        if (c == 52  or c == 97):
             rect_x = rect_x - moving_size_fast
 
         if (c == 27):
             break
 
-        if(c == 13):
+
+        if(c == 13 or c == 32):
             source_image = cv.imread(cv.samples.findFile(source_image_path), cv.IMREAD_COLOR)
             crop_img = source_image[rect_y:rect_y+rect_h, rect_x:rect_x+rect_w]
             cv.imwrite(os.path.join(target_folder, str(target_counter) + ".png"), crop_img)
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Install Python 3.6.8
 Install older version of python: 
 python-3.6.8-amd64.exe https://www.python.org/downloads/release/python-368/
+
+# Install Dependencies
+pip install opencv-python
+pip install matplotlib
+pip install tensorflow==1.8.0
+pip install keras==2.1.5
+pip install sklearn
+pip install pandas
